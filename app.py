a8c324ff0c3f50397bdefb1081d2b0030a75e865
Kaleb Fenner <167553759+BadVibesLLC@users.noreply.github.com>
2024-07-06 17:58:49 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
 def main():
-    st.components.v1.iframe("https://www.google.com/")
+    st.components.v1.html("<script async src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9369193555413965,
+     crossorigin='anonymous'></script>")
 
 if __name__ == "__main__":
     main()
