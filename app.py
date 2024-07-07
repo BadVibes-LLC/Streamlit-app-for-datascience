c4fa42262931238078f20a35c060cdd1fb333f7d
Kaleb Fenner <167553759+BadVibesLLC@users.noreply.github.com>
2024-07-06 19:17:26 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import streamlit as st
 
 def main():
-    st.components.v1.iframe("https://web-games.streamlit.app/ads.txt")
+    st.components.v1.html('''
+    <html>
+    <meta name="google-adsense-account" content="ca-pub-9369193555413965">
+    </html>
+    ''')
     st.write("Testing page")
 
 if __name__ == "__main__":
