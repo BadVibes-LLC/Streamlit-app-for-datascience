cbc3e176501b059c0e902a597b64555ac8fc6c33
Kaleb Fenner <167553759+BadVibesLLC@users.noreply.github.com>
2024-07-06 18:01:35 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import streamlit as st
 
+
+html = '''
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9369193555413965"
+     crossorigin="anonymous"></script>
+'''
 def main():
-    st.components.v1.html('<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9369193555413965",
-     crossorigin="anonymous"></script>')
+    st.components.v1.html(html)
 
 if __name__ == "__main__":
     main()
