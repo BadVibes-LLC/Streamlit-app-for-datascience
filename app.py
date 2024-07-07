683e11e57f179e04a1a35e00a4ec553d7b41a47f
Kaleb Fenner <167553759+BadVibesLLC@users.noreply.github.com>
2024-07-06 18:02:08 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ html = '''
 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9369193555413965"
      crossorigin="anonymous"></script>
 '''
+
 def main():
     st.components.v1.html(html)
 
