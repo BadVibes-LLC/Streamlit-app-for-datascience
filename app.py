760e0f00cfcf1ab886f5b2d486333c71f2b7f8d3
Kaleb Fenner <167553759+BadVibesLLC@users.noreply.github.com>
2024-07-06 19:22:50 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ import streamlit as st
 def main():
     st.components.v1.html('''
     <html>
+    <head>
     <meta name="google-adsense-account" content="ca-pub-9369193555413965">
+    </head>
     </html>
     ''')
     st.write("Testing page")
