8570ea9003aedd0665081f7ab265adfc565097bc
Kaleb Fenner <167553759+BadVibesLLC@users.noreply.github.com>
2024-07-06 19:29:33 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ import streamlit as st
 def main():
     st.html('''
     <html>
+    <head>
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9369193555413965"
      crossorigin="anonymous"></script>
+     </head>
      </html>
     ''')
     st.write("Testing page")
