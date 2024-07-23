23af8d07a7d3513bf7f91e132773c703340d568d
Kaleb Fenner <167553759+BadVibesLLC@users.noreply.github.com>
2024-07-23 14:18:44 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,7 @@
 import streamlit as st
 
 def main():
-    st.html('''
-    <html>
-    <head>
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9369193555413965"
-     crossorigin="anonymous"></script>
-     </head>
-     </html>
-    ''')
-    st.write("Testing page")
+    pass
 
 if __name__ == "__main__":
     main()
