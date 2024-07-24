4f68dd1a82e4c6303e3bae1e9c5a2f1b38a7fb32
BadVibesLLC <badvibes.lllc@gmail.com>
2024-07-24 12:44:48 -0600

    .

--- pages/Generative_AI_Example.py
+++ pages/Generative_AI_Example.py
@@ -3,29 +3,19 @@ from openai import OpenAI
 
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
-def process_stream(stream):
-  for response in stream:
-    content = response["choices"][0]["delta"].get("content")
-  if content:
-    return content
-  else:
-    return ""
-
 def main():
   st.title("Generative AI Example using OpenAI and LangChain")
   input = st.text_input("Ask me anything!", key="input")
 
   if input:
-    stream = client.chat.completions.create(
+    st.write_stream(stream = client.chat.completions.create(
       model="gpt-4o-mini",
       messages=[
         {"role": "system", "content": "You are an helpful assistant."},
         {"role": "user", "content": input},
         ],
       stream=True
-    )
-
-    st.write_stream(process_stream(stream))
+    ))
 
 if __name__ == "__main__":
   main()
