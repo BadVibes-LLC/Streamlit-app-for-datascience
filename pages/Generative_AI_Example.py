c1fcc33ed99c19b3801f74be5106bc030ab18615
BadVibesLLC <badvibes.lllc@gmail.com>
2024-07-24 12:55:48 -0600

    .

--- pages/Generative_AI_Example.py
+++ pages/Generative_AI_Example.py
@@ -5,9 +5,11 @@ client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 def main():
   st.title("Generative AI Example using OpenAI and LangChain")
+  
   input = st.text_input("Ask me anything!", key="input")
 
   if input:
+    st.session_state["input"] = ""
     st.write_stream(stream = client.chat.completions.create(
       model="gpt-4o-mini",
       messages=[
