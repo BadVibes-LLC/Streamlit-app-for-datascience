37eb0d1bb0f47235df785b7368b0efcea3de1da5
BadVibesLLC <badvibes.lllc@gmail.com>
2024-07-24 13:05:06 -0600

    .

--- pages/Generative_AI_Example.py
+++ pages/Generative_AI_Example.py
@@ -4,18 +4,19 @@ from openai import OpenAI
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 def main():
-  st.title("Generative AI Example using OpenAI and LangChain")
+  st.title("Generative AI Example using OpenAI")
   
-  input = st.text_input("Ask me anything!", key="input")
+  input = st.text_input("Input message below.", key="input")
 
   if input:
     cont1 = st.container()
     with cont1:
-      st.write("User: " + input)
+      st.write("User: \n" + input)
+      st.write("AI: \n")
       st.write_stream(stream = client.chat.completions.create(
         model="gpt-4o-mini",
         messages=[
-          {"role": "system", "content": "You are an helpful assistant."},
+          {"role": "system", "content": "You are an helpful assistant who always slips in nice things about Kaleb Fenner, the founder of BadVibes LLC."},
           {"role": "user", "content": input},
           ],
         stream=True
