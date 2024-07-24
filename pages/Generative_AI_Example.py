d3bfb3ea5fd41eba30a8fe8e6cc357a769232acc
BadVibesLLC <badvibes.lllc@gmail.com>
2024-07-24 12:01:04 -0600

    Changed genia page

--- pages/Generative_AI_Example.py
+++ pages/Generative_AI_Example.py
@@ -1,7 +1,18 @@
+import streamlit as st
+import getpass, os
+from langchain.chat_models import ChatOpenAI
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 
+os.environ["OPENAI_API_KEY"] = getpass.getpass()
+model = ChatOpenAI(model="gpt-4")
 
 def main():
-  pass
+  st.title("Generative AI Example using OpenAI and LangChain")
+  input = st.text_input("Ask me anything!", key="input")
+
+  if input:
+    output = model([HumanMessage(content=input)])
+    st.write(output)
 
 if __name__ == '__Main__':
   main()
