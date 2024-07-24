cafea93cd7a66cf6af2e987eed91e924ad8de5d5
BadVibesLLC <badvibes.lllc@gmail.com>
2024-07-24 11:39:49 -0600

    Updated main page

--- BadVibesLLC_AI_Showcase.py
+++ BadVibesLLC_AI_Showcase.py
@@ -1,1 +1,12 @@
+import streamlit as st
+import pandas as pd
 
+def train():
+    pass
+
+def main():
+    st.title('BadVibes LLC AI Showcase')
+    st.write("This is the AI showcase of BadVibes LLC developed by Kaleb Fenner.")
+
+if __name__ == '__main__':
+    main()
