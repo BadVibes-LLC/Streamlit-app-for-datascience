c1636245d3a1ea98789be4c7be57a41027561603
Kaleb Fenner <167553759+BadVibesLLC@users.noreply.github.com>
2024-07-24 15:18:32 -0600

    Update BadVibesLLC_AI_Showcase.py

--- BadVibesLLC_AI_Showcase.py
+++ BadVibesLLC_AI_Showcase.py
@@ -7,6 +7,7 @@ def train():
 def main():
     st.title('BadVibes LLC AI Showcase')
     st.write("This is a showcase of Kaleb Fenner's python skills in generative AI and machine learning.")
+    st.write("It features a chatbot powered by OpenAI, the proximal policy optimization network, and a pytorch implementation of a different machine learning algorithm.")
 
 if __name__ == '__main__':
     main()
