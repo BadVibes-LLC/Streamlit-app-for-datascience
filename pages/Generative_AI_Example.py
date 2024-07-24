50a880568f9c990abf5782c0abd97ec48d4d53ef
BadVibesLLC <badvibes.lllc@gmail.com>
2024-07-24 12:23:18 -0600

    Added chat thing

--- pages/Generative_AI_Example.py
+++ pages/Generative_AI_Example.py
@@ -1,18 +1,29 @@
 import streamlit as st
 import getpass, os
-from langchain.chat_models import ChatOpenAI
-from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from openai import OpenAI
 
-os.environ["OPENAI_API_KEY"] = getpass.getpass()
-model = ChatOpenAI(model="gpt-4")
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
 
 def main():
+
+
   st.title("Generative AI Example using OpenAI and LangChain")
   input = st.text_input("Ask me anything!", key="input")
 
   if input:
-    output = model([HumanMessage(content=input)])
-    st.write(output)
+    stream = client.chat.completions.create(
+      model="gpt-4o-mini",
+      messages=[
+        {"role": "system", "content": "You are an helpful assistant."},
+        {"role": "user", "content": input},
+        ],
+      stream=True
+    )
+
+    for response in stream:
+      if response.choices[0].delta.content is not None:
+        st.write_stream(response.choices[0].delta.content, end="")
 
 if __name__ == '__Main__':
   main()
